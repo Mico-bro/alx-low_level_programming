cf3ccd088a6945074b95c0e1827c8cde4226c86b
mico-art <micoyiftu@gmail.com>
2022-06-03 16:16:10 +0300

    C_hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
-// printing txt file 
-#include <stdio.h>
+ #include <stdio.h>
 
 int main (void)
+/**
+ * main - Prints using puts
+ * Return: Always 0.
+ */
 {
 puts("\"Programming is like building a multilingual puzzle\n");
 return(0);
