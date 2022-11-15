45f615a32baa0ec58ad732346ab698bc05914903
mico-art <micoyiftu@gmail.com>
2022-11-15 15:44:10 +0300

    new

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,32 @@
 #ifndef DOG_H
 #define DOG_H
-/**
- *struct dog - a new type
- *@name : string
- *@age : float
- *@owner : string
- *Description: a new type that contains dog name, age, and owner.
+
+/*
+ * File: dog.h
+ * Desc: Header file that defines a new type struct dog.
  */
-typedef struct dog
+
+/**
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
